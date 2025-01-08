9499e76ed3f5a1fa32e6d1c1cbbfa1322dde5bf9
Ashwini Pachore <ashwinipachore494@gmail.com>
2025-01-08 17:56:58 +0530

    typecasting and typeconversion

---
+++ Typecasting.java
@@ -0,0 +1,8 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        double doublevalue = 15.4;
+        int intvalue = (int) doublevalue;
+        System.out.println("doublevalue is:" + doublevalue);
+        System.out.println("intvalue is :" + intvalue);
+    }
+}
---
+++ Typeconversion.java
@@ -0,0 +1,9 @@
+public class Typeconversion {
+    public static void main(String[] args) {
+        int intvalue =100;
+        double doublevalue = intvalue ;
+        System.out.println("intvalue is:" + intvalue);
+        System.out.println("doublevalue is:" +  doublevalue);
+    }
+    
+}
