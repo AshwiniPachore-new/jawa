ee82004ba94a5c3180eafa26ac28bd01b0ca9c66
Ashwini Pachore <ashwinipachore494@gmail.com>
2025-01-08 12:49:14 +0530

    unwanted files

--- whileloop.java
+++ whileloop.java
@@ -1,6 +1,6 @@
 public class Whileloop{
     public static void main(String[] args){
-        int i=;
+        int i=1;
         while( i<=5){
             System.out.println("its small number"+i);
             i++;
